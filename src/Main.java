dcf38f0864e12775ca107a94ecbdc4a6c0ad5d35
jooyom <908joo@gamil.com>
2024-06-03 02:52:39 +0900

    0602

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-float myFloat = 9.75f;
+        float myFloat = 9.75f;
         float anotherFloat = myFloat * 2;
-        System.out.println("Original float value: " + (int)myFloat);
-        System.out.println("Calculated float value: " + (byte)anotherFloat);
+        System.out.println("Original float value: " + (int) myFloat);
+        System.out.println("Calculated float value: " + (byte) anotherFloat);
 
-
-boolean isTure = true;
+        boolean isTure = true;
 
         if (isTure) {
             System.out.println("이건트루입니다.");
         } else {
-                System.out.println("이건펄스입니다.");
+            System.out.println("이건펄스입니다.");
         }
-
-
 
 
         int[] arr = {1, 2, 3, 4, 5}; //안에 값은 정수
@@ -25,19 +24,22 @@ boolean isTure = true;
         int[] arr2 = new int[5];
         System.out.println(arr[0]);
 
- String name = "berserker";
+        String name = "berserker";
         String name2 = new String("berserker"); //이렇게 해도 같다.
-        String[] arr3 = {"d", "d", "e"};
-        String[] arr4 = new String[3];
-
 
         System.out.println(name);
         System.out.println(name2);
 
+
+        //Arrays 도 씀
+        List<Integer> list = Arrays.asList(1, 2, 3); //Integer는 숫자형 String은 문자형
+        //배열을 List로 바꾸는데 사용
+        List<String> li = new ArrayList<>();
+
+        System.out.println(list.get(0));
+        int ii = 10;
+
+        int num1 = 123_456_789;
+       // int num2 = 123,456,789; 쉼표는 사용할 수 없다
     }
 }
-
- /* Arrays //도 씀
-    List<Integer> list = Arrays.asList(1);
-        System.out.println(list.get(0));
-        int a = 10;*/
--- src/May31.java
+++ src/May31.java
@@ -3,17 +3,20 @@ import javax.lang.model.element.NestingKind;
 public class May31 {
     public static void main(String[] args) {
 
+        int[] arrr = new int[100];
+        System.out.println(arrr);
+
         String firstName = "Joo Young";
         String secondName = "Kim";
         String phone = "iphone";
         String status = "sleepy";
         System.out.println("나는" + secondName + " " + firstName + "입니다.");
-        System.out.println("내" + phone+ "은" + status);
+        System.out.println("내" + phone + "은" + status);
 
-        System.out.println(firstName=secondName);
-    String a = "hello";
-    String  b = new String("berserker");
-    System.out.println(a.equals(b)); //true
+        System.out.println(firstName = secondName);
+        String a = "hello";
+        String b = new String("berserker");
+        System.out.println(a.equals(b)); //true
         System.out.println(a == b); //false 메모리 주소값을 비교
 
         String aa = "Hello World!";
@@ -24,23 +27,25 @@ public class May31 {
         System.out.println(bb.contains("SOFT")); //대소문자를 구분하니 유의
 
         String findChar = "What is your hobby";
-        System.out.println(findChar.charAt(13) + "" + findChar.charAt(14) + "" + findChar.charAt(15) + "" + findChar.charAt(16) + "" + findChar.charAt(17) + "");
+        System.out.println(findChar.charAt(13) + "" + findChar.charAt(14) + findChar.charAt(15) + findChar.charAt(16) + findChar.charAt(17));
 
         String d = "Hello World!";
         /*System.out.println(d.replaceAll());*/
 
         String dd = "WEEEE ASKE ;ND"; //끝위치는 +1할것!
-        System.out.println(d.substring(0, 3) + d.substring(8, 10) + d.substring(12, 14));
-        System.out.println(d.substring(0,d.length()-1));
+        System.out.println(dd.substring(0, 3) + dd.substring(8, 10));
+        System.out.println(dd.substring(3));
+        System.out.println(dd.substring(0, dd.length() - 1));
+
 
         String e = "a:b:c:d";
         e.split(":"); //split에서 알트엔터 하면 아래처럼 나옴 지역변수 삽입
-         String[] split = e.split(":");
+        String[] split = e.split(":");
 
 
-         String result = "Hello";
+        String result = "Hello";
 
-        System.out.println(result+"!");
+        System.out.println(result + "!");
         result.concat("!");
 
         String name1 = "Tim";
@@ -53,8 +58,8 @@ public class May31 {
         String.format("%s의 나이는 %s세 입니다", name2, age2);
 
         String result2 = "";
-        result2  += "Hello";
-        result2  += "java";
+        result2 += "Hello";
+        result2 += "java";
         result2 += "World";
         System.out.println(result2);
         StringBuilder sb = new StringBuilder();
@@ -81,25 +86,26 @@ public class May31 {
         int[] arr = new int[100]; //길이가 100인 배열
         String[] arr2 = new String[1000];  //천개의 문자를 받는 배열
         System.out.println(arr); //배열의 주소가 나오게됨
-        System.out.println(arr[0]); //0
+        System.out.println(arr[2]); //0
         System.out.println(arr2[0]); //null 값이 없어서
 
         //배열의 가장 큰 값 구하기
-        int [] arr3 = {1, 2, 3};
+        int[] arr3 = {1, 2, 3};
         System.out.println(arr[3]); //out of bounds 범위 초과 에러남
 
 
         int positive = 10;
         int negative = -10;
 
-        System.out.println("양의 부호 연산자 적용" + (+positive));
-        System.out.println("음의 부호 연산자 적용" + (-positive));
-        System.out.println("음의 부호 연산자로 부호 반전" + (-negative));
+        System.out.println("양의 부호 연산자 적용" + (+positive)); //10
+        System.out.println("음의 부호 연산자 적용" + (-positive)); //-10
+        System.out.println("음의 부호 연산자로 부호 반전" + (+negative)); //-10
+        System.out.println("음의 부호 연산자로 부호 재반전" + (-negative)); //10
 
         int x = 5;
         int y = 5;
-        System.out.println("전위 증가 연산자: "+ (++x)); //6
-        System.out.println("후위 증가 연산자: "+ (y++)); //5
+        System.out.println("전위 증가 연산자: " + (++x)); //6
+        System.out.println("후위 증가 연산자: " + (y++)); //5
         System.out.println("======================");
         System.out.println("x는" + x); //6
         System.out.println("y는" + y); //6
@@ -107,28 +113,31 @@ public class May31 {
 
         int A = 100;
         int B = 100;
-        System.out.println("전위 감소 연산자: "+ (--A)); //99
-        System.out.println("후위 감소 연산자: "+ (B--)); //100
-        System.out.println("전위 감소 연산자: "+ (--A)); //98
-        System.out.println("후위 감소 연산자: "+ (B--)); //99
-        System.out.println("전위 감소 연산자: "+ (--A)); //97
-        System.out.println("후위 감소 연산자: "+ (B--)); //98
+        System.out.println("전위 감소 연산자: " + (--A)); //99
+        System.out.println("후위 감소 연산자: " + (B--)); //100
+        System.out.println("전위 감소 연산자: " + (--A)); //98
+        System.out.println("후위 감소 연산자: " + (B--)); //99
+        System.out.println("전위 감소 연산자: " + (--A)); //97
+        System.out.println("후위 감소 연산자: " + (B--)); //98
 
         boolean isRaining = true;
         if (isRaining) {
             System.out.println("하늘에서 비가와요");
         }
-        if (isRaining){
+        if (!isRaining) {
             System.out.println("?");
         }
         boolean isSunny = false;
-        if (isSunny) {
+        if (!isSunny) {
             System.out.println("날씨가 맑아요");
-        }else {
+        } else {
             System.out.println("집에서 쉬어야죠");
         }
 
-
-
     }
 }
+
+
+
+
+
